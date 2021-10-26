e3b6d768526c94786aa49afb1ba0324ea653def8
Louis Ardilly <louis.ardilly@ign.fr>
2021-10-26 12:35:40 +0200

    recuperation de la dalle avec la requete wms

--- app/controllers/pcrs.py
+++ app/controllers/pcrs.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-
+from flask import Blueprint, render_template, request, redirect, url_for, send_file
+from owslib.wms import WebMapService
+import requests
+import os
 
 pcrs = Blueprint('pcrs', __name__, url_prefix='/')
-
 
 @pcrs.route('/')
 def index():
@@ -11,6 +12,14 @@ def index():
 
 @pcrs.route('/version1')
 def version1():
+    # creation du dossier img si il n'existe pas
+    os.makedirs("app/static/img", exist_ok=True)
+    # on stocke l'image dans un dossier en la recuperant avec une requete wms avec sa bbox, avant de lui faire ses traitements
+    requete_wms((244000,6736400,244200,6736600))
+    
+    # if os.path.isfile("app/static/img/image.tif"):
+    #     file = os.path.abspath("app/static/img/image.tif")
+    #     return send_file(file)
     return render_template('pages/version1.html')
 
 
@@ -18,3 +27,31 @@ def version1():
 def version2():
     return render_template('pages/version2.html')
 
+
+def log_wms_serveur():
+    """ connexion au serveur wms"""
+    os.environ['https_proxy'] = "http://proxy.ign.fr:3128"
+    os.environ['http_proxy'] = "http://proxy.ign.fr:3128"
+    os.environ['HTTPS_PROXY'] = "http://proxy.ign.fr:3128"
+    os.environ['HTTP_PROXYS'] = "http://proxy.ign.fr:3128"
+    wms = WebMapService('https://vectortiles.ign.fr/wms', version='1.3.0')
+    return wms
+
+def requete_wms(bbox):
+    """ on recupere la dalle à l'aide de la bbox, et on execute le requete wms, et on save l'img dans le dossier temporairement
+    
+    bbox(tuple): bbox d'une dalle
+    """
+    wms = log_wms_serveur()
+    dalle = wms.getmap(
+        layers=['PCRS'],
+        format='image/tiff',
+        srs='EPSG:2154',
+        bbox=bbox,
+        style=[],
+        size=(1000,1000)
+        )
+    
+    img = open('app/static/img/test.tif', 'wb')
+    img.write(dalle.read())
+    img.close
