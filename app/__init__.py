78add29248064318279bbb88710c92a884e3f37e
LArdilly <louis.ardilly@ign.fr>
2022-07-29 16:06:43 +0200

    ajout du telechargement de la grille lidarhd via shp ou geojson

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from app.controllers import pcrs, api
+from app.controllers import pcrs, api, download_lidar
 
 
 app = Flask(__name__)
 
 app.register_blueprint(pcrs.pcrs)
 app.register_blueprint(api.api)
+app.register_blueprint(download_lidar.download_lidar)
